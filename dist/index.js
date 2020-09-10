d1cca81943c61b746a045d1b78a995cc2400c209
Atanas Atanasov <scriptex.bg@gmail.com>
2020-09-10 14:33:30 +0300

    Fix missing method call

--- dist/index.js
+++ dist/index.js
@@ -3,7 +3,7 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.default = exports.Donut = void 0;
+exports["default"] = exports.Donut = void 0;
 
 var _react = _interopRequireDefault(require("react"));
 
@@ -11,7 +11,7 @@ var _propTypes = _interopRequireDefault(require("prop-types"));
 
 require("./index.css");
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
 function _extends() { _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; }; return _extends.apply(this, arguments); }
 
@@ -29,7 +29,7 @@ var getClassName = function getClassName(p, c) {
 };
 
 var renderProgress = function renderProgress(progress) {
-  return _react.default.createElement("strong", null, progress);
+  return /*#__PURE__*/_react["default"].createElement("strong", null, progress);
 };
 
 var Donut = function Donut(_ref) {
@@ -39,29 +39,29 @@ var Donut = function Donut(_ref) {
       onRender = _ref$onRender === void 0 ? renderProgress : _ref$onRender,
       _ref$prefix = _ref.prefix,
       prefix = _ref$prefix === void 0 ? PREFIX : _ref$prefix;
-  return _react.default.createElement("div", {
+  return /*#__PURE__*/_react["default"].createElement("div", {
     className: getClassName(prefix, progress < 0 ? ' is--negative' : '')
-  }, _react.default.createElement("svg", {
+  }, /*#__PURE__*/_react["default"].createElement("svg", {
     className: getClassName(prefix, '__canvas'),
     width: SIZE,
     height: SIZE,
     viewBox: "0 0 ".concat(SIZE, " ").concat(SIZE),
     xmlns: "http://www.w3.org/2000/svg"
-  }, _react.default.createElement("circle", _extends({
+  }, /*#__PURE__*/_react["default"].createElement("circle", _extends({
     className: getClassName(prefix, '__frame')
-  }, circleProps)), _react.default.createElement("circle", _extends({
+  }, circleProps)), /*#__PURE__*/_react["default"].createElement("circle", _extends({
     className: getClassName(prefix, '__circle'),
     strokeDasharray: "".concat(Math.abs(progress), " 100")
-  }, circleProps))), typeof onRender === 'function' && _react.default.createElement("div", {
+  }, circleProps))), typeof onRender === 'function' && /*#__PURE__*/_react["default"].createElement("div", {
     className: getClassName(prefix, '__text')
   }, onRender(progress)));
 };
 
 exports.Donut = Donut;
 Donut.propTypes = {
-  progress: _propTypes.default.number,
-  onRender: _propTypes.default.func,
-  prefix: _propTypes.default.string
+  progress: _propTypes["default"].number,
+  onRender: _propTypes["default"].func,
+  prefix: _propTypes["default"].string
 };
 Donut.defaultProps = {
   progress: 0,
@@ -69,4 +69,4 @@ Donut.defaultProps = {
   prefix: PREFIX
 };
 var _default = Donut;
-exports.default = _default;
+exports["default"] = _default;
